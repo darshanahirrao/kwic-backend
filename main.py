dee77d6d30b717686b96f3899f9f79961d54e846
Darshan Ahirrao <92846082+darshanahirrao@users.noreply.github.com>
2024-04-18 13:07:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -68,4 +68,4 @@ async def generate_schedule(query: Query):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1",port=8000)
+    uvicorn.run(app, host="0.0.0.0",port=10000)
